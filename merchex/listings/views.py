bef3053c2ec43659cea729d499691bd51d5d55b8
Arthur40480 <arthurgibert@free.fr>
2023-03-16 17:14:11 +0100

    Add models

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,8 +1,27 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from listings.models import Band
+from listings.models import Announcement
 
 def hello(request):
-    return HttpResponse('<h1>Hello Django !</h1>')
+    bands = Band.objects.all()
+    announcements = Announcement.objects.all()
+    return HttpResponse(f"""
+        <h1>Hello Django !</h1>
+        <p>Mes groupes préférés sont :
+        <ul>
+            <li>{bands[0].name}</li>
+            <li>{bands[1].name}</li>
+            <li>{bands[2].name}</li>
+        </ul>
+        
+        <p> Voici les différentes annonces :</p>
+        <ul>
+            <li>{announcements[0].title}</li>
+            <li>{announcements[1].title}</li>
+            <li>{announcements[2].title}</li>
+        </ul>
+""")
 
 def about(request):
     return HttpResponse('<h1>À propos</h1> <p>Nous adorons merch!</p>')
