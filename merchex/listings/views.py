f2dadb5a59d7c3f23dd4dbf7dabcf1dbfc490a70
Arthur40480 <arthurgibert@free.fr>
2023-03-22 15:21:41 +0100

    Add CRUD interface

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,8 +1,9 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 from django.shortcuts import render
 from listings.models import Band
 from listings.models import Announcement
-from listings.forms import ContactUsForm
+from listings.forms import ContactUsForm, BandForm, AnnouncementForm
 from django.core.mail import send_mail
 
 
@@ -17,12 +18,48 @@ def band_detail(request, id):
     return render(request,
                   'listings/band_detail.html',
                   {'band': band})
+def create_new_band(request):
+    if request.method == 'POST':
+        form = BandForm(request.POST)
+        if form.is_valid():
+            band = form.save()
+            return HttpResponseRedirect(reverse('band-detail', kwargs={'id': band.id}))
+    else:
+        form = BandForm()
+
+    return render(request,
+                  'listings/band_add.html',
+                  {'form': form})
+def band_update(request, id):
+    band = Band.objects.get(id=id)
+
+    if request.method == 'POST':
+        form = BandForm(request.POST, instance=band)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('band-detail', kwargs={'id': band.id}))
+    else:
+        form = BandForm(instance=band)
+
+    return render(request,
+                  'listings/band_update.html',
+                  {'form': form})
+
+def band_delete(request, id):
+    band = Band.objects.get(id=id)
+    if request.method == 'POST':
+        band.delete()
+        return HttpResponseRedirect(reverse('band-list'))
+
+    return render(request,
+                  'listings/band_delete.html',
+                  {'band': band})
 
 def about(request):
     return render(request,
                   'listings/about.html')
 
-def listings(request):
+def listings_list(request):
     announcements = Announcement.objects.all()
     return render(request,
                   'listings/listings.html',
@@ -34,6 +71,42 @@ def listings_detail(request, id):
                   'listings/listings_detail.html',
                   {'announcement': announcement})
 
+def create_new_listing(request):
+    if request.method == 'POST':
+        form = AnnouncementForm(request.POST)
+        if form.is_valid():
+            announcement = form.save()
+            return HttpResponseRedirect(reverse('listing-detail', kwargs={'id': announcement.id}))
+    else:
+        form = AnnouncementForm()
+
+    return render(request,
+                  'listings/listings_add.html',
+                  {'form': form})
+
+def listing_update(request, id):
+    listing = Announcement.objects.get(id=id)
+    if request.method == 'POST':
+        form = AnnouncementForm(request.POST, instance=listing)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('listing-detail', kwargs={'id': listing.id}))
+    else:
+        form = AnnouncementForm(instance=listing)
+
+    return render(request,
+                  'listings/listing_update.html',
+                  {'form': form})
+
+def listing_delete(request, id):
+    listing = Announcement.objects.get(id=id)
+    if request.method == 'POST':
+        listing.delete()
+        return HttpResponseRedirect(reverse('listing-list'))
+
+    return render(request,
+                  'listings/listing_delete.html',
+                  {'listing': listing})
 def contact(request):
     if request.method == 'POST':
         form = ContactUsForm(request.POST)
